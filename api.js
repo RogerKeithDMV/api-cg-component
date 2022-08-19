0fdf08cca9bf4267c3ddab2879449f41ee3927b1
Daniel Maturano Vazquez <101372967+RogerKeithDMV@users.noreply.github.com>
2022-08-19 11:36:29 -0500

    update package

---
+++ api.js
@@ -0,0 +1,27 @@
+const rabbitmq = require('msgbroker-nxg-cg');
+const {log, constants} = require('utils-nxg-cg');
+const {api} = require('api-cg-lib');
+const {emits} = constants;
+
+
+/**
+ * Method for api connections
+ * @param msg
+ * @param cfg
+ * @param snapshot
+ * @returns {Promise<void>}
+ */
+ module.exports.process = async function apiProcess(msg, cfg, snapshot = {}) {
+    try {
+        const _data = await api(msg, cfg);
+        this.emit(emits.data, {data: _data});
+        snapshot.lastUpdated = new Date();
+        this.emit(emits.snapshot, snapshot);
+        log.info(constants.FINISH_EXEC);
+        this.emit(emits.end);
+    } catch (e) {
+        log.error(e);
+        this.emit(emits.error, e);
+        await rabbitmq.producerErrorMessage(msg.toString(), e.toString());
+    }
+};
